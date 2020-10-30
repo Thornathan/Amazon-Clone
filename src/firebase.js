02ac2f8ff52255d29ca58bb3d00ee7acaefe4e8c
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-10-30 04:11:55 -0700

    Firebase Added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDEuc0Yw2yMjBTCDncqO4jmOE2dVjMW1Io",
+  authDomain: "clone-fb90e.firebaseapp.com",
+  databaseURL: "https://clone-fb90e.firebaseio.com",
+  projectId: "clone-fb90e",
+  storageBucket: "clone-fb90e.appspot.com",
+  messagingSenderId: "1096963285775",
+  appId: "1:1096963285775:web:ee14d4f7ccefcc3389947a",
+  measurementId: "G-9K5TXKTGP3",
+};
